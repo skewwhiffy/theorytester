8706d1f76ea2f7f7652827ae3d4dd84dbb8ee4ce
Kenny Hung (he) <kennysubs+github@outlook.com>
2024-11-03 14:07:12 +0000

    A bit nicer

--- backend/PaperMusic/Interval.fs
+++ backend/PaperMusic/Interval.fs
@@ -41,35 +41,63 @@ and INonPerfectFactory =
     abstract Sixth: Interval
     abstract Seventh: Interval
 
-and WrappingPerfectIntervalFactory(wrapped: IPerfectFactory, transform: Interval -> Interval) =
-    interface IPerfectFactory with
-        member this.Unison = transform wrapped.Unison
-        member this.Fourth = transform wrapped.Fourth
-        member this.Fifth = transform wrapped.Fifth
-        member this.Octave = transform wrapped.Octave
-
-and WrappingNonPerfectIntervalFactory(wrapped: INonPerfectFactory, transform: Interval -> Interval) =
-    interface INonPerfectFactory with
-        member this.Second = transform wrapped.Second
-        member this.Third = transform wrapped.Third
-        member this.Sixth = transform wrapped.Sixth
-        member this.Seventh = transform wrapped.Seventh
-
 and Interval(size: int, quality: IntervalQuality) =
-    static member Perfect: IPerfectFactory = PerfectFactory()
-    static member Major: INonPerfectFactory = MajorFactory()
+    static member wrap(wrapped: INonPerfectFactory, transform: Interval -> Interval) =
+        { new INonPerfectFactory with
+            member this.Second = transform wrapped.Second
+            member this.Third = transform wrapped.Third
+            member this.Sixth = transform wrapped.Sixth
+            member this.Seventh = transform wrapped.Seventh }
 
-    static member Minor: INonPerfectFactory =
-        WrappingNonPerfectIntervalFactory(Interval.Major, _.Diminish())
+    static member wrap(wrapped: IPerfectFactory, transform: Interval -> Interval) =
+        { new IPerfectFactory with
+            member this.Unison = transform wrapped.Unison
+            member this.Fourth = transform wrapped.Fourth
+            member this.Fifth = transform wrapped.Fifth
+            member this.Octave = transform wrapped.Octave }
 
-    static member Diminished: IFactory = DiminishedFactory()
-    static member Augmented: IFactory = AugmentedFactory()
+    static member wrap(perfect: IPerfectFactory, nonPerfect: INonPerfectFactory) =
+        { new IFactory with
+            member this.Unison = perfect.Unison
+            member this.Second = nonPerfect.Second
+            member this.Third = nonPerfect.Third
+            member this.Fourth = perfect.Fourth
+            member this.Fifth = perfect.Fifth
+            member this.Sixth = nonPerfect.Sixth
+            member this.Seventh = nonPerfect.Seventh
+            member this.Octave = perfect.Octave }
+
+    static member Perfect =
+        let perfect = IntervalQuality(0)
+
+        { new IPerfectFactory with
+            member this.Unison = Interval(1, perfect)
+            member this.Fourth = Interval(4, perfect)
+            member this.Fifth = Interval(5, perfect)
+            member this.Octave = Interval(8, perfect) }
+
+    static member Major =
+        let major = IntervalQuality(0)
+
+        { new INonPerfectFactory with
+            member this.Second = Interval(2, major)
+            member this.Third = Interval(3, major)
+            member this.Sixth = Interval(6, major)
+            member this.Seventh = Interval(7, major) }
+
+
+    static member Minor = Interval.wrap (Interval.Major, _.Diminish())
+
+    static member Diminished =
+        Interval.wrap (Interval.wrap (Interval.Perfect, _.Diminish()), Interval.wrap (Interval.Minor, _.Diminish()))
+
+    static member Augmented =
+        Interval.wrap (Interval.wrap (Interval.Perfect, _.Augment()), Interval.wrap (Interval.Major, _.Augment()))
 
     member this.Size = size
     member this.Quality = quality
     member this.Diminish() = Interval(size, quality.Diminished)
     member this.Augment() = Interval(size, quality.Augmented)
-
 
     override this.Equals other =
         match other with
@@ -77,59 +105,6 @@ and Interval(size: int, quality: IntervalQuality) =
         | _ -> false
 
     override this.GetHashCode() = HashCode.Combine(size, quality)
-
-
-and MajorFactory() =
-    member private this.Major = IntervalQuality(0)
-
-    interface INonPerfectFactory with
-        member this.Second = Interval(2, this.Major)
-        member this.Third = Interval(3, this.Major)
-        member this.Sixth = Interval(6, this.Major)
-        member this.Seventh = Interval(7, this.Major)
-
-and PerfectFactory() =
-    member private this.Perfect = IntervalQuality(0)
-
-    interface IPerfectFactory with
-        member this.Unison = Interval(1, this.Perfect)
-        member this.Fourth = Interval(4, this.Perfect)
-        member this.Fifth = Interval(5, this.Perfect)
-        member this.Octave = Interval(8, this.Perfect)
-
-and DiminishedFactory() =
-    static member private perfect: IPerfectFactory =
-        WrappingPerfectIntervalFactory(Interval.Perfect, _.Diminish())
-
-    static member private minor: INonPerfectFactory =
-        WrappingNonPerfectIntervalFactory(Interval.Minor, _.Diminish())
-
-    interface IFactory with
-        member this.Unison = DiminishedFactory.perfect.Unison
-        member this.Second = DiminishedFactory.minor.Second
-        member this.Third = DiminishedFactory.minor.Third
-        member this.Fourth = DiminishedFactory.perfect.Fourth
-        member this.Fifth = DiminishedFactory.perfect.Fifth
-        member this.Sixth = DiminishedFactory.minor.Sixth
-        member this.Seventh = DiminishedFactory.minor.Seventh
-        member this.Octave = DiminishedFactory.perfect.Octave
-
-and AugmentedFactory() =
-    static member private perfect: IPerfectFactory =
-        WrappingPerfectIntervalFactory(Interval.Perfect, _.Augment())
-
-    static member private major: INonPerfectFactory =
-        WrappingNonPerfectIntervalFactory(Interval.Major, _.Augment())
-
-    interface IFactory with
-        member this.Unison = AugmentedFactory.perfect.Unison
-        member this.Second = AugmentedFactory.major.Second
-        member this.Third = AugmentedFactory.major.Third
-        member this.Fourth = AugmentedFactory.perfect.Fourth
-        member this.Fifth = AugmentedFactory.perfect.Fifth
-        member this.Sixth = AugmentedFactory.major.Sixth
-        member this.Seventh = AugmentedFactory.major.Seventh
-        member this.Octave = AugmentedFactory.perfect.Octave
 
 type Interval with
     [<JsonIgnore>]
